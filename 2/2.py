08512d32a96bed743e91a80512f3236733491b53
Arhydemon <1692328@gmail.com>
2026-02-25 16:54:59 +0300

    2 лаба

--- 2/2.py
+++ 2/2.py
@@ -26,18 +26,32 @@ class Block: # класс блок самый главный
         block_string = json.dumps(block_data, sort_keys = True).encode()
         return hashlib.sha256(block_string).hexdigest() # .hexdigest превращает его в удобную строку из букв и цифр:
 
-def create_blockchainik(): # тута создаём цепочку из 5 блоков
+    def mine(self, difficulty): 
+        target="0" * difficulty
+        self.nonce = 0
+
+        while True:
+            self.hash = self.calculate_hash()
+            if self.hash[:difficulty] == target:
+                break
+            else:
+                self.nonce += 1 
+
+def create_blockchainik(difficulty): # тута создаём цепочку из 5 блоков
     chain = [] # список где будут храниться ВСЕ блоки
     genesis = Block(0, time.time(), "Нулевой блок", "0")
+    genesis.mine(difficulty) # майним нулевой блок, чтобы он был валидным, иначе вся цепочка будет сломана
     chain.append(genesis) # добавляем первый блок в список от греческого «γένεσις» = происхождение, рождение, возникновение как Книга Бытия в Библии
 
     for i in range(1,5): # создаём еще 4 блока чтобы было 5
         pred_block = chain[-1]
         new_block = Block(i, time.time(), f"Инфа для {i} блока", pred_block.hash)
+        new_block.mine(difficulty)
         chain.append(new_block)
     return chain
 
-def is_chain_valid(chain):
+def is_chain_valid(chain, difficulty): # проверка целостности цепочки, если кто-то изменит данные в блоке, то хеш изменится и цепочка будет сломана
+    target = "0" * difficulty # проверка каждого блока начиная со второго, так как нулевой блок не на что не ссылается
     for i in range (1, len(chain)):
         current = chain[i] #текущий блок
         previous = chain[i-1]
@@ -45,9 +59,13 @@ def is_chain_valid(chain):
             return False # если хеш изменился то цепочка сломана
         if current.previous_hash != previous.hash:
             return False # если ссылка неправильная то цепочка сломана
+        if not current.hash.startswith(target):
+            return False # если блок не майнится то цепочка сломана
+    if not chain[0].hash.startswith(target):
+        return False # если нулевой блок не майнится то цепочка сломана
     return True
 
-def print_chain(chain):
+def print_chain(chain): # вывод всех блоков в цепочке, для наглядности
     for block in chain:
         print("_" * 60)
         print('index:', block.index)
@@ -57,7 +75,23 @@ def print_chain(chain):
         print('nonce', block.nonce)
         print('hash', block.hash)
 
+def report():
+    print("ОТЧЁТ ПО БЛОКЧЕЙНУ:")
+    print("=" * 40)
+
+    for difficulty in [1,2,3,4,5]: # проверяем разные сложности майнинга
+        start = time.perf_counter() # замер времени, perf_counter() возвращает время в секундах с высокой точностью, идеально для измерения времени выполнения кода
+        chain = create_blockchainik(difficulty)
+        end = time.perf_counter() # замер времени окончания
+        total_time = end - start
+        avg_time = total_time / len(chain)
+
+        print(f"Сложность: {difficulty}, Время: {total_time:.2f} секунд, Среднее время: {avg_time:.2f} секунд, Проверка PoW: {is_chain_valid(chain, difficulty)}")        
+
 if __name__ == "__main__":
-    blockchain = create_blockchainik() # создание блокчейна
+    difficulty = 4 # сложность майнинга, чем выше тем дольше майнится блок
+    blockchain = create_blockchainik(difficulty) # создание блокчейна
     print_chain(blockchain) # вывод всех блоков
-    print('Целостность цепочечки?', is_chain_valid(blockchain)) # проверка целостности цепочки
+    if is_chain_valid(blockchain, difficulty): # проверка целостности цепочки
+        print('Целостность цепочечки:', is_chain_valid(blockchain, difficulty))
+    report()
